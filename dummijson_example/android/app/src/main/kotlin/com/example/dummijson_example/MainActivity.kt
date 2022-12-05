b3c2201ad4c0c88e89d13a1e67b1a994098db144
Sproute777 <prestiz4321@gmail.com>
2022-12-05 20:30:42 +0300

    dummijson migration #6

--- dummijson_example/android/app/src/main/kotlin/com/example/dummijson_example/MainActivity.kt
+++ dummijson_example/android/app/src/main/kotlin/com/example/dummijson_example/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.fakestore_api
+package com.example.dummijson_example
 
 import io.flutter.embedding.android.FlutterActivity
 
--- fakestore_api/README.md
+++
@@ -1,6 +0,0 @@
-# fakestore_api
-
-клиент для https://fakestoreapi.com/
-
-архитектура bloc 
-
